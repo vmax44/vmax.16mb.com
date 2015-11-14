72b9fc33eab04939186054141e5a803110364216
vmax44 <vmax44@tut.by>
2015-11-14 20:35:27 +0300

    freshdesk ready

---
+++ freshdesk/pass-sample.php
@@ -0,0 +1,15 @@
+<?PHP
+
+$loginData = array(	
+	// accountname.freshdesk.com
+	"account"=>"accountname",
+	"login"=>"test@jkj.com",
+	"password"=>"12345678",
+	
+	//mysql
+	"mysql_db"=>"dbname",
+	"mysql_host"=>"localhost",
+	"mysql_username"=>"user",
+	"mysql_password"=>"password"
+);
+?>
